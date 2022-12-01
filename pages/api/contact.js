5169c43d153575192cd7a3a917d21bae0a0243cc
Annapari <83932523+Annapari@users.noreply.github.com>
2022-12-02 00:35:27 +1300

    Update contact.js
    Signed-off-by: Annapari <83932523+Annapari@users.noreply.github.com>

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -15,7 +15,7 @@ export default function (req, res) {
   })
   const mailData = {
     from: 'emailfromkatevox@gmail.com',
-    to: 'annaparis.phillipson@gmail.com',
+    to: 'insertyouremail@gmail.com',
     subject: `Contact Form submission from ${req.body.firstname}`,
     text: req.body.message + ' | Sent from ' + req.body.email,
     html: `<div>${req.body.message}</div>
